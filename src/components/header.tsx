36322fedc7145c7608ecac9560016a828929518b
cherrydub <cherrydub@gmail.com>
2024-04-02 21:19:04 +0100

    adding layout to the page, also adding whitelist for image link in next.config.mjs

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
+import Logo from "./logo";
 
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header>
+      <Logo />
+      <Link href="/">Home</Link>
+      <Link href="/events/all">All Events</Link>
+    </header>
+  );
 }
---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Image
+      width={53}
+      height={12}
+      alt="Logo"
+      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+    />
+  );
+}
