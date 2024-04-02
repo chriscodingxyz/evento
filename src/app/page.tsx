51cef419eee0efa9fd1876706d73c57c1a647052
cherrydub <cherrydub@gmail.com>
2024-04-03 00:14:15 +0100

    adding styles to footer and making variable for routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center pt-36 px-3">
       <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
-        Find events aroudn you
+        Find events around you
       </h1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
 import React from "react";
 
+const routes = [
+  {
+    name: "Terms & Conditions",
+    path: "/terms-conditions",
+  },
+  {
+    name: "Privacy Policy",
+    path: "/privacy-policy",
+  },
+];
+
 export default function Footer() {
-  return <footer className="mt-auto">footer</footer>;
+  return (
+    <footer className="mt-auto flex items-center justify-between  h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25">
+      <small className="text-xs">
+        &copy; 2022 Bytegrad. All rights reserved.
+      </small>
+      <ul className="flex gap-x-3 sm:gap-x-8">
+        {routes.map(({ name, path }) => (
+          <li key={path}>
+            <Link href={path}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
