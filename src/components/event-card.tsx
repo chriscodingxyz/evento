838fee39353f3625689cc3fe784b8e32214cac84
cherrydub <cherrydub@gmail.com>
2024-04-03 23:13:17 +0100

    flexbox responsive design, using flex-1 along with basis and a max width

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,6 +1,27 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 import React from "react";
 
 export default function EventCard({ event }: { event: EventoEvent }) {
-  return <section>{event.name}</section>;
+  return (
+    <section
+      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px]
+    
+    
+    bg-white/[3%] rounded-xl overflow-hidden"
+    >
+      <Image
+        className="h-[60%] object-fit"
+        src={event.imageUrl}
+        alt={event.name}
+        width={500}
+        height={280}
+      />
+      <div className="flex flex-col justify-center items-center flex-1">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">{event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">{event.location}</p>
+      </div>
+    </section>
+  );
 }
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -4,7 +4,7 @@ import EventCard from "./event-card";
 
 export default function EventsList({ events }: { events: EventoEvent[] }) {
   return (
-    <section>
+    <section className=" max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
