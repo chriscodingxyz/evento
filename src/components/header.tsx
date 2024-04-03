254c2a4b251b5809c5de9fef9989314b5286d7d5
cherrydub <cherrydub@gmail.com>
2024-04-03 12:44:04 +0100

    conditionally adding div for active pathnames, will now style with framer motion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,17 +22,23 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9  ">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 h-full text-sm">
           {routes.map(({ name, path }) => (
             <li
               key={path}
-              className={clsx("hover:text-white transition", {
-                "text-white border-b border-accent": activePathname === path,
-                "text-white/50": activePathname !== path,
-              })}
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white border-b border-accent": activePathname === path,
+                  "text-white/50": activePathname !== path,
+                }
+              )}
             >
               <Link href={path}>{name}</Link>
+              {activePathname === path && (
+                <div className="bg-accent h-1 w-full absolute bottom-0"></div>
+              )}
             </li>
           ))}
         </ul>
