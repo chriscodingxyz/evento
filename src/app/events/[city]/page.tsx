8079065709ba087e95a4de56cc6abd8c719cc1e1
cherrydub <cherrydub@gmail.com>
2024-04-03 21:16:43 +0100

    instead of using usePathname, next comes with "params" as a prop, used this method for this dynamic page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,13 +1,19 @@
 import H1 from "@/components/h1";
 import React from "react";
 
-export default function EventsPage() {
+export default function EventsPage({ params }: { params: { city: string } }) {
+  const city = params.city;
+  const cityCamel = city.charAt(0).toUpperCase() + city.slice(1);
+
   return (
     <main
       className="flex flex-col items-center justify-center
     py-24 px-[20px] min-h-[110vh]"
     >
-      <H1>Events in Austin</H1>
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" && `Events in ${cityCamel}`}
+      </H1>
     </main>
   );
 }
