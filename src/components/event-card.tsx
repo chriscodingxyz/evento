d7d8b2baaa23f8106f2367067441a0a0986a41b7
cherrydub <cherrydub@gmail.com>
2024-04-03 23:24:47 +0100

    utilizing new Date to format the days with a leading 0 and the month

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -4,12 +4,7 @@ import React from "react";
 
 export default function EventCard({ event }: { event: EventoEvent }) {
   return (
-    <section
-      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px]
-    
-    
-    bg-white/[3%] rounded-xl overflow-hidden"
-    >
+    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden relative">
       <Image
         className="h-[60%] object-fit"
         src={event.imageUrl}
@@ -22,6 +17,20 @@ export default function EventCard({ event }: { event: EventoEvent }) {
         <p className="italic text-white/75">{event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{event.location}</p>
       </div>
+      <section
+        className="absolute left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md
+      flex flex-col justify-center items-center"
+      >
+        <p className="text-xl font-bold -mb-[5x]">
+          {new Date(event.date).getDate().toString().padStart(2, "0")}
+          {/* {new Date(event.date).toLocaleDateString("en-US", { day: "2-digit" })} */}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            month: "short",
+          })}
+        </p>
+      </section>
     </section>
   );
 }
