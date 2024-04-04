0d3189a1a7d7826201e89262685e724d66c186c8
cherrydub <cherrydub@gmail.com>
2024-04-04 16:13:11 +0100

    since next.js aggressively caches, you can add a no-cache, but more appropriately you can add a time for it to re-validate, add an object after the fetch url

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -3,9 +3,13 @@ import React from "react";
 import EventCard from "./event-card";
 
 export default async function EventsList({ city }: { city: string }) {
-  //fetch
   const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
+    {
+      next: {
+        revalidate: 300,
+      },
+    }
   );
   const dataEvents: EventoEvent[] = await response.json();
   return (
