0f81ddfe33ff360087372b0b0a3160495467a891
cherrydub <cherrydub@gmail.com>
2024-04-04 13:00:52 +0100

    responsive design added to single event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/components/h1";
 import Image from "next/image";
 import React from "react";
 
@@ -14,7 +15,7 @@ export default async function EventPage({
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
           src={dataEvent.imageUrl}
           alt={dataEvent.name}
@@ -24,13 +25,32 @@ export default async function EventPage({
           sizes="(max-width: 1280px) 100vw, 1280px"
           priority
         />
-        <div className="z-1 relative">
+        <div className="z-1 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
           <Image
+            className="rounded-xl border-2 border-white/50 object-cover"
             src={dataEvent.imageUrl}
             alt={dataEvent.name}
             width={300}
             height={201}
           />
+          <div className=" flex flex-col">
+            <p className=" text-white/75">
+              {new Date(dataEvent.date).toLocaleString("default", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {dataEvent.name}
+            </H1>
+            <p className="whitepsace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{dataEvent.organizer}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] sm:w-full py-2 border-white/10  border-2 hover:scale-105 active:scale-[1.02] focus:scale-105 transition">
+              Get Tickets
+            </button>
+          </div>
         </div>
       </section>
       <div></div>
