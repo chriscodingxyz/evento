dcfb080e6444df1ebdc52ac1548514386e352352
cherrydub <cherrydub@gmail.com>
2024-04-04 11:10:01 +0100

    adding event image as a blurred background, optimizing with fill instead of width/height. Also using zindexes and optimizing with priority tag for image

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export default async function EventPage({
@@ -9,7 +10,30 @@ export default async function EventPage({
   const resposne = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
-  const data = await resposne.json();
+  const dataEvent = await resposne.json();
 
-  return <main>eventslug page</main>;
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={dataEvent.imageUrl}
+          alt={dataEvent.name}
+          className="object-cover blur-3xl z-0"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+        <div className="z-1 relative">
+          <Image
+            src={dataEvent.imageUrl}
+            alt={dataEvent.name}
+            width={300}
+            height={201}
+          />
+        </div>
+      </section>
+      <div></div>
+    </main>
+  );
 }
