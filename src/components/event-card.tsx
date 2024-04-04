4997b9dda15941bb10cc4dcf41dea9a9b1d2bbc6
cherrydub <cherrydub@gmail.com>
2024-04-04 17:55:00 +0100

    wrapping a Link using motion. use client is needed for this component due to framer motion and how it builds these animations. also using ref to determine when the bottom of the view will meet the next mapped item, from there we can add offset options. Also including initial opacity/scale for the new MotionLink

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import { EventoEvent } from "@/lib/types";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+
+const MotionLink = motion(Link);
 
 export default function EventCard({ event }: { event: EventoEvent }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
   return (
-    <Link
+    <MotionLink
+      ref={ref}
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
+      style={{
+        // @ts-ignore
+        scale: scaleProgress,
+        // @ts-ignore
+        opacity: opacityProgress,
+      }}
+      initial={{ scale: 0.8, opacity: 0 }}
     >
       <section className="w-full h-full flex flex-col  bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
@@ -37,6 +59,6 @@ export default function EventCard({ event }: { event: EventoEvent }) {
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
