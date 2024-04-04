42b03aaa22fff0cbedf89c8c8606b51575dae710
cherrydub <cherrydub@gmail.com>
2024-04-04 14:53:17 +0100

    utilized cn function for combining clsx and twmerge, this allows for different className widths/heights for the Skeletons

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -4,9 +4,9 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="flex flex-col items-center gap-y-4 pt-28">
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
+      <Skeleton className="h-4 w-[550px]" />
+      <Skeleton className="h-4 w-[400px]" />
+      <Skeleton className="h-4 w-[430px]" />
     </div>
   );
 }
--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,5 +1,13 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-export default function Skeleton() {
-  return <div className="animate-pulse h-4 w-[550px] rounded-md bg-white/5" />;
+export default function Skeleton({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "animate-pulse h-4 w-[550px] rounded-md bg-white/5",
+        className
+      )}
+    />
+  );
 }
