c20cab0f94bb78d2d3bc125d5915366f71bf1681
cherrydub <cherrydub@gmail.com>
2024-04-05 13:26:54 +0100

    added orderBy for the evens to sort by date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -35,6 +35,9 @@ export async function getEvents(city: string) {
     where: {
       city: city === "all" ? undefined : capitalizeFirst(city),
     },
+    orderBy: {
+      date: "asc",
+    },
   });
 
   return events;
