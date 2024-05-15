290c654749a1a45c562ac5a7dc7c5caad39e7f69
cherrydub <cherrydub@gmail.com>
2024-05-15 01:05:34 +0100

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
   return (
     <footer className="mt-auto flex items-center justify-between  h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25">
       <small className="text-xs">
-        &copy; 2022 Bytegrad. All rights reserved.
+        &copy; {new Date().getFullYear()} chriscoding.xyz. All rights reserved.
       </small>
       <ul className="flex gap-x-3 sm:gap-x-8">
         {routes.map(({ name, path }) => (
